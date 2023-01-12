6d09c1f38ea76ee9477b00db3d9831c137e2a3c8
Jake-NotTheMuss <jakenotthemuss@gmail.com>
2023-01-12 02:59:55 -0800

    Add Analyzer code

---
+++ src/lanalyzer.c
@@ -0,0 +1,52 @@
+/*
+** $Id: lanalyzer.c $
+** Auxiliary functions to manipulate prototype analyzer structures
+** See Copyright Notice in lua.h
+*/
+
+
+#include <stddef.h>
+
+#define lanalyzer_c
+#define LUA_CORE
+
+#include "lua.h"
+
+#include "lanalyzer.h"
+#include "lgc.h"
+#include "lmem.h"
+#include "lobject.h"
+#include "lstate.h"
+#include "lstring.h"
+
+#ifdef HKSC_DECOMPILER
+
+Analyzer *luaA_newanalyzer (hksc_State *H) {
+  Analyzer *a = luaM_new(H, Analyzer);
+  luaC_link(H, obj2gco(a), LUA_TANALYZER);
+  a->insproperties = NULL;
+  a->sizeinsproperties = 0;
+  a->regproperties = NULL;
+  a->sizeregproperties = 0;
+  a->locvars = NULL;
+  a->sizelocvars = 0;
+  a->bbldata = NULL;
+  a->sizebbldata = 0;
+  return a;
+}
+
+
+void luaA_freeanalyzer (hksc_State *H, Analyzer *a) {
+  int i;
+  luaM_freearray(H, a->insproperties, a->sizeinsproperties, InstructionFlags);
+  luaM_freearray(H, a->regproperties, a->sizeregproperties, RegisterFlags);
+  luaM_freearray(H, a->locvars, a->sizelocvars, struct LocVar);
+  for (i = 0; i < a->sizebbldata; i++) {
+    struct BBLStart *bbl = &a->bbldata[i];
+    luaM_freearray(H, bbl->types, bbl->sizetypes, lu_byte);
+  }
+  luaM_freearray(H, a->bbldata, a->sizebbldata, struct BBLStart);
+  luaM_free(H, a);
+}
+
+#endif /* HKSC_DECOMPILER */
---
+++ src/lanalyzer.h
@@ -0,0 +1,121 @@
+/*
+** $Id: lanalyzer.h $
+** Auxiliary functions to manipulate code analyzer structures
+** See Copyright Notice in lua.h
+*/
+
+#ifndef lanalyzer_h
+#define lanalyzer_h
+
+#ifdef HKSC_DECOMPILER
+
+#include "lobject.h"
+
+#if defined(ldecomp_c) || defined(lanalyzer_c)
+
+/*
+** basic block types
+*/
+#define BBLTYPE_TABLE \
+  DEFBBLTYPE(FUNCTION)   /* a Lua function */      \
+  DEFBBLTYPE(DO)         /* a do-block */          \
+  DEFBBLTYPE(WHILE)      /* a while-loop */        \
+  DEFBBLTYPE(REPEAT)     /* a repeat-loop */       \
+  DEFBBLTYPE(FORNUM)     /* a for-number-loop */   \
+  DEFBBLTYPE(FORLIST)    /* a for-list-loop */     \
+  DEFBBLTYPE(IF)         /* an if-block */         \
+  DEFBBLTYPE(ELSEIF)     /* an elseif-block */     \
+  DEFBBLTYPE(ELSE)       /* an else-block */
+
+#define DEFBBLTYPE(e)  BBL_##e,
+enum BBLTYPE {
+  BBLTYPE_TABLE
+  BBL_MAX,
+  /* special pass 1 values */
+  BBL1_FOR = BBL_FORNUM,
+  BBL1_MAX = BBL1_FOR + 1
+};
+#undef DEFBBLTYPE
+
+
+/*
+** instruction properties
+*/
+#define INSFLAG_TABLE \
+  DEFINSFLAG(FJT)  \
+  DEFINSFLAG(BJT)  \
+  DEFINSFLAG(PRECONCAT)  /* first pc that sets up a concat operation */ \
+  DEFINSFLAG(PRECALL)  /* first pc that sets up a function call */ \
+  DEFINSFLAG(PRERETURN)  /* first pc that evaluates a returned expression */ \
+  /* possible first pc that evaluates a single return value */ \
+  DEFINSFLAG(PRERETURN1)  \
+  DEFINSFLAG(PREBRANCHTEST)  /* first pc that evaluates a branch condition */ \
+  /* possible first pc that evaluates a branch condition */ \
+  DEFINSFLAG(PREBRANCHTEST1) \
+  /* false-jump out of an if-statement condition evaluation */ \
+  DEFINSFLAG(BRANCHFAIL) \
+   /* true-jump out of an if-statement condition evaluation */ \
+  DEFINSFLAG(BRANCHPASS) \
+  DEFINSFLAG(PRELOOPTEST)  /* first pc that evaluates a loop condition */ \
+  /* possible first pc that evaluates a loop condition */ \
+  DEFINSFLAG(PRELOOPTEST1) \
+  DEFINSFLAG(LOOPFAIL)  /* false-jump out of a loop condition evaluation */ \
+  DEFINSFLAG(LOOPPASS)  /* true-jump out of a loop condition evaluation */ \
+  DEFINSFLAG(OPTLOOPFAILEXIT)  /* optimized jump target of a loop fail */ \
+  DEFINSFLAG(REPEATSTAT)  /* first pc in a repeat-loop */ \
+  DEFINSFLAG(WHILESTAT)  /* first pc in a while-loop */ \
+  DEFINSFLAG(WHILEEXIT)  /* a jump instruction in a while-loop condition */ \
+  DEFINSFLAG(IFSTAT)  /* first pc in an if-branch */ \
+  DEFINSFLAG(ELSEIFSTAT)  /* first pc in an elseif-branch */ \
+  DEFINSFLAG(ELSESTAT)  /* first pc in an else-branch */ \
+  DEFINSFLAG(IFSTATEND)  /* exit target of an if-statement */ \
+  DEFINSFLAG(FORLIST)  /* first pc in a list for-loop */ \
+  /* first pc to evaluate for-list control variables */ \
+  DEFINSFLAG(PREFORLIST) \
+  DEFINSFLAG(FORNUM)  /* first pc in a numeric for-loop */ \
+  DEFINSFLAG(PREFORNUM)  /* first pc to evluate for-num control variables */ \
+  DEFINSFLAG(BLOCKEND)  /* last pc in a block */ \
+  DEFINSFLAG(LOOPEND)  /* last pc in a loop */ \
+  DEFINSFLAG(BREAKSTAT)  /* pc is a break instruction */ \
+  DEFINSFLAG(DOSTAT)  /* pc begins a block */
+
+#define DEFINSFLAG(e)  INS_##e,
+enum INSFLAG {
+  INSFLAG_TABLE
+  MAX_INSFLAG
+};
+#undef DEFINSFLAG
+
+
+/*
+** register properties
+*/
+#define REGFLAG_TABLE \
+  DEFREGFLAG(FREE)      /* a free register */ \
+  DEFREGFLAG(RESERVED)  /* a register being used in a temporary expression */ \
+  DEFREGFLAG(LOCAL)     /* a register which holds an active local variable */ \
+  DEFREGFLAG(CONTROL)   /* a register which holds a loop control variable */ \
+  DEFREGFLAG(UPVAL)     /* a register used as an upvalue */
+
+#define DEFREGFLAG(e)  REG_##e,
+enum REGFLAG {
+  REGFLAG_TABLE
+  MAX_REGFLAG
+};
+#undef DEFREGFLAG
+
+
+struct BBLStart {
+  lu_byte *types;  /* the vector of basic block types that start here */
+  int pc;  /* startpc of the block */
+  int ntypes;  /* number of blocks that start here */
+  int sizetypes;
+};
+
+#endif /* ldecomp_c */
+
+LUAI_FUNC Analyzer *luaA_newanalyzer (hksc_State *H);
+LUAI_FUNC void luaA_freeanalyzer (hksc_State *H, Analyzer *a);
+
+#endif /* HKSC_DECOMPILER */
+#endif
