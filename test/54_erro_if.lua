f0618815404a51dc2104d20b6dbf028bf62787d3
Jake-NotTheMuss <jakenotthemuss@gmail.com>
2023-03-31 17:53:17 -0700

    update 54_erro_if.lua test

--- test/54_erro_if.lua
+++ test/54_erro_if.lua
@@ -1,7 +1,7 @@
 -- trick the decompiler into detecting an if-block that isn't there, and test
 -- its ability to recover
 local a,b;
-a = a() or b or a or d
+a = b or a or d
 
 if a then
 	return a;
