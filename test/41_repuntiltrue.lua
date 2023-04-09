5447b27e952ebd66f3aefb8a27d056d8172a6e1a
Jake-NotTheMuss <jakenotthemuss@gmail.com>
2023-04-08 17:09:12 -0700

    update tests

--- test/41_repuntiltrue.lua
+++ test/41_repuntiltrue.lua
@@ -2,7 +2,7 @@
 	-- 2	[3]	LOADK    	1 -2	; 5678
 	-- 3	[4]	RETURN   	0 1
 
--- this is not detected as a basic block because it emits no jumps
+-- this is not detected as a loop because it emits no jumps
 repeat
 	local a = 1234;
 	local b = 5678;
--- test/47_rep_break_until_true.lua
+++ test/47_rep_break_until_true.lua
@@ -1,3 +1,12 @@
+--[[
+decompiler will call see this as:
+
+if false then
+end
+
+which is sementically equivalent and generates the same code
+]]
+
 repeat
 	break;
 until true;
