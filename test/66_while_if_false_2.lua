4f43b8ce53ff05490bd7a811f8dd677cf28a9871
Jake-NotTheMuss <jakenotthemuss@gmail.com>
2023-04-12 17:49:45 -0700

    update and add test files

---
+++ test/66_while_if_false_2.lua
@@ -0,0 +1,23 @@
+while a do
+	local a = 12;
+	if false then
+		local a = 12;
+	end
+	if false then
+		local b = 34;
+	end
+	if false then
+		local c= 56;
+	end
+	local d = 78;
+	if false then
+		local a = 12;
+	end
+	if false then
+		local b = 34;
+	end
+	if false then
+		local c= 56;
+	end
+	return a;
+end
