8b7578786e50ee502a37d90c536073b9d9ebf18f
Jake-NotTheMuss <jakenotthemuss@gmail.com>
2023-05-01 19:20:24 -0700

    add test

---
+++ test/89_loadnil_before_openexpr.lua
@@ -0,0 +1,13 @@
+-- test the decompiler's ability to handle an OP_LOADNIL that is shared
+-- between multiple statements
+
+local y = nil;
+y = nil .. nil;
+local a, c, b;
+
+local a = nil;
+a = (nil)();
+
+a = nil;
+
+a = (nil)(nil, nil, nil);
